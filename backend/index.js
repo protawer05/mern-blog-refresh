7506e96284022fb5ac24d4f19944a289ba5693db
Anton <sychiov2015@yandex.ru>
2024-01-17 08:12:17 +0500

    did little backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import { registerValidation } from './validations/auth.js'
 import { validationResult } from 'express-validator'
 import UserModel from './models/User.js'
 import bcrypt from 'bcrypt'
+import checkAuth from './utils/checkAuth.js'
 const app = express()
 mongoose
 	.connect('mongodb+srv://gasper:220767@cluster0.jj7c0xv.mongodb.net/blog?retryWrites=true&w=majority')
@@ -40,7 +41,41 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 		res.json({ msg: err }).status(500)
 	}
 })
+app.post('/auth/login', async (req, res) => {
+	try {
+		const user = await UserModel.findOne({ email: req.body.email })
+		if (!user) {
+			return res.status(404).json({ message: 'Пользователь не найден' })
+		}
 
+		const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+		if (!isValidPass) {
+			return res.status(404).json({ message: 'Неверный логин или пароль' })
+		}
+		const token = jwt.sign({ _id: user._id }, 'secret123', { expiresIn: '30d' })
+
+		const { passwordHash, ...userData } = user._doc
+
+		res.json({ ...userData, token })
+	} catch (err) {
+		console.log(err)
+		res.status(403).json({ message: 'Не удалось авторизоваться' })
+	}
+})
+app.get('/auth/me', checkAuth, async (req, res) => {
+	try {
+		const user = await UserModel.findOne({ _id: req.userId })
+		if (!user) {
+			res.status(404).json({ message: 'Пользователь не найден' })
+		}
+
+		const { passwordHash, ...userData } = user._doc
+
+		res.json({ ...userData })
+	} catch (err) {
+		return res.status(400).json({ message: 'Не удалось получить информацию' })
+	}
+})
 app.listen(4444, err => {
 	err ? console.log(err) : console.log('server is ok')
 })
